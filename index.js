6c3908e3cf284a0e1588284a497e14184807f700
quaziyadgar <quaziyadgar@gmail.com>
2025-04-17 15:11:45 +0530

    update in index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import videoRoutes from './routes/videos.js';
 import authRoutes from './routes/auth.js';
@@ -11,10 +11,13 @@ dotenv.config();
 
 const app = express();
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-// }));
+app.options('*', cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 app.use('/api/videos', videoRoutes);
