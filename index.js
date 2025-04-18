b1c030b6732dc15a1d8cede70cf32be4f2d29c89
quaziyadgar <quaziyadgar@gmail.com>
2025-04-18 14:19:23 +0530

    cors configured

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  console.log(`${req.method} ${req.url} from ${req.headers.origin}`);
+  next();
+});
+
 app.use(express.json());
 app.use('/api/videos', videoRoutes);
 app.use('/api/auth', authRoutes);
