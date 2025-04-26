ae61139c3294b3c1acd5e8d539740d11a8667240
quaziyadgar <quaziyadgar@gmail.com>
2025-04-26 14:28:44 +0530

    temp

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
     const token = jwt.sign({ userId: user._id, username, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(201).json({ token, user: {_id, username, email } });
+    res.status(201).json({ token, user: {_id:user._id, username, email } });
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ error: 'Invalid credentials' });
     }
     const token = jwt.sign({ userId: user._id, username: user.username, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token, user: { username: user.username, email: user.email } });
+    res.json({ token, user: {_id:user._id, username: user.username, email: user.email } });
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
