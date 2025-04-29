a7656bf343a9bd86a033adad3eb6517fdde9a8b3
quaziyadgar <quaziyadgar@gmail.com>
2025-04-29 15:13:04 +0530

    added origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://youtube-clone-bice-pi.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
